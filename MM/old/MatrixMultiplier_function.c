268f4813875fdcdef0d1419772848c5f2ee9b7ec
FEBRR <101262433+FEBRR@users.noreply.github.com>
2025-05-26 21:56:51 +0200

    Delete MM/old directory

--- MM/old/MatrixMultiplier_function.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "my_first_function.h"
-
-int main(void) {
-    printf("Starting matrix multiplication...\n");
-    performMatrixMultiplication();
-    return 0;
-}
--- MM/old/MatrixMultiplier_function.h
+++
@@ -1,7 +0,0 @@
-#ifndef MY_FIRST_FUNCTION_H
-#define MY_FIRST_FUNCTION_H
-
-void performMatrixMultiplication(void);
-
-#endif
-
